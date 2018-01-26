f5bd90b87c6a9b1ae94292839bb283ae73cfc65d
Simon Andersson <si8270an-s@student.lu.se>
2018-01-26 13:43:15 +0100

    Add files via upload

--- print.h
+++ print.h
@@ -27,20 +27,20 @@ uint32_t parse_int(unsigned char *buf, long val) {
         buf[pos++] = '-';
         val = -val;
     }
-    while(val) {
+    do {
         buf[pos++] = (unsigned char)((val%10) + '0');
         val /= 10;
-    }
+    } while(val);
     reverse(buf+sign, buf+pos-1);
     return pos;
 }
 
 static inline uint32_t parse_uint(unsigned char *buf, unsigned long val) {
     uint32_t pos = 0;
-    while(val) {
+    do {
         buf[pos++] = (unsigned char)((val%10) + '0');
         val /= 10;
-    }
+    } while(val);
     reverse(buf, buf+pos-1);
     return pos;
 }
@@ -48,10 +48,10 @@ static inline uint32_t parse_uint(unsigned char *buf, unsigned long val) {
 uint32_t parse_uint_hex(unsigned char *buf, unsigned long val) {
     static unsigned char hexdigits[16] = "0123456789ABCDEF";
     uint32_t pos = 0;
-    while(val) {
+    do {
         buf[pos++] = hexdigits[val&15];
         val >>= 4;
-    }
+    } while(val);
     reverse(buf, buf+pos-1);
     return pos;
 }
@@ -68,17 +68,7 @@ static inline uint32_t uintlog10(uint32_t val) {
             val >= 10        ? 1 : 0;
 }
 
-static inline double pow_10(uint32_t e) {
-    return e == 0 ? 1 :
-            e == 1 ? 10 :
-            e == 2 ? 100 :
-            e == 3 ? 1000 :
-            e == 4 ? 10000 :
-            e == 5 ? 100000 :
-            e == 6 ? 1000000 :
-            e == 7 ? 10000000 :
-            e == 8 ? 100000000 : 1000000000;
-}
+const double pow_10[10] = { 1.0, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001, 0.000000001 };
 
 uint32_t parse_double(unsigned char *buf, double val) {
     uint32_t pos = 0;
@@ -94,23 +84,20 @@ uint32_t parse_double(unsigned char *buf, double val) {
         exponent -= 9;
     } 
     while(val >= 1.0e10) {
-        val /= 1000000000.0;
+        val *= 0.000000001;
         exponent += 9;
     }
     u = val;
     uint32_t e = uintlog10(u);
     exponent += e;
-    val /= pow_10(e);
+    val *= pow_10[e];
     u = val;
     buf[pos++] = u%10 + '0';
     buf[pos++] = '.';
     val -= u;
-    if(val) {
-        val *= 1000000000.0;
-        u = (uint32_t)val;
-        val -= u;
-        pos += parse_uint(buf+pos, u);
-    }
+    val *= 1000000000.0;
+    u = val;
+    pos += parse_uint(buf+pos, u);
     if(exponent) {
         buf[pos++] = 'e';
         if(*(uint32_t*)&exponent >> 31){
@@ -122,55 +109,90 @@ uint32_t parse_double(unsigned char *buf, double val) {
     return pos;
 }
 
-void print(const char *format, ... ) {
+#define haszero64(x) (((x) - 0x0101010101010101UL) & ~(x) & 0x8080808080808080UL)
+#define hasmod64(x) haszero64(x ^ 0x2525252525252525UL)
+#define align64(x) (((uintptr_t)x & 7) == 0)
+
+#define haszero32(x) (((x) - 0x01010101U) & ~(x) & 0x80808080U)
+#define hasmod32(x) haszero32(x ^ 0x25252525U)
+#define align32(x) (((uintptr_t)x & 3) == 0)
+
+#define haszero16(x) (((x) - 0x0101) & ~(x) & 0x8080)
+#define hasmod16(x) haszero16(x ^ 0x2525)
+#define align16(x) (((uintptr_t)x & 1) == 0)
+
+typedef union DuoPtr {
+    unsigned char *c;
+    uint16_t *s;
+    uint32_t *u;
+    uint64_t *l;
+} DuoPtr;
+
+typedef union ConstDuoPtr {
+    const unsigned char *c;
+    const uint16_t *s;
+    const uint32_t *u;
+    const uint64_t *l;
+} ConstDuoPtr;
+
+void print(const char *format, ...) {
     static unsigned char buf[BUFFER_SIZE];
+    DuoPtr _buf;
+    ConstDuoPtr _fmt;
+    _buf.c = buf;
+    _fmt.c = (const unsigned char*)format;
+    const unsigned char *str;
+    double dbl;
     va_list args;
     va_start(args, format);
-    uint32_t pos = 0;
-    for(uint32_t i=0; format[i]; ++i) {
-        if(pos >= BUFFER_SIZE-50) {
-            write(STDOUT, buf, pos);
-            pos = 0;
+    while(1) {
+        if(align64(_buf.c) && align64(_fmt.c)) {
+            while(!haszero64(*_fmt.l) && !hasmod64(*_fmt.l)) *_buf.l++ = *_fmt.l++;
+        } else if(align32(_buf.c) && align32(_fmt.c)) {
+            while(!haszero32(*_fmt.u) && !hasmod32(*_fmt.u)) *_buf.u++ = *_fmt.u++;
+        } else if(align16(_buf.c) && align16(_fmt.c)) {
+            while(!haszero16(*_fmt.s) && !hasmod32(*_fmt.s)) *_buf.s++ = *_fmt.s++;
         }
-        if(format[i] != '%') buf[pos++] = format[i];
-        else {
-            const unsigned char *str;
-            switch(format[++i]) {
-                case 'c':
-                    buf[pos++] = va_arg(args, int);
-                    break;
-                case 's':
-                    str = va_arg(args, const unsigned char*);
-                    while(*str) buf[pos++] = *str++;
-                    break;
-                case 'd': case 'i':
-                    pos += parse_int(buf+pos, va_arg(args, int));
-                    break;
-                case 'D': case 'I':
-                    pos += parse_int(buf+pos, va_arg(args, long));
-                    break;
-                case 'x':
-                    pos += parse_uint_hex(buf+pos, va_arg(args, unsigned int));
-                    break;
-                case 'X':
-                    pos += parse_uint_hex(buf+pos, va_arg(args, unsigned long));
-                    break;
-                case 'u':
-                    pos += parse_uint(buf+pos, va_arg(args, unsigned int));
-                    break;
-                case 'U':
-                    pos += parse_uint(buf+pos, va_arg(args, unsigned long));
-                    break;
-                case 'f': case 'F':
-                    pos += parse_double(buf+pos, va_arg(args, double));
-                    break; 
-                case '%':
-                    buf[pos++] = '%';
-                    break;
-            }
+        while(*_fmt.c && *_fmt.c != '%') *_buf.c++ = *_fmt.c++;
+        if(!*_fmt.c) break;
+        switch(*++_fmt.c) {
+            case 'c':
+                *_buf.c++ = va_arg(args, int);
+                break;
+            case 's':
+                str = va_arg(args, const unsigned char*);
+                while(*str) *_buf.c++ = *str++;
+                break;
+            case 'd': case 'i':
+                _buf.c += parse_int(_buf.c, va_arg(args, int));
+                break;
+            case 'D': case 'I':
+                _buf.c += parse_int(_buf.c, va_arg(args, long));
+                break;
+            case 'x':
+                _buf.c += parse_uint_hex(_buf.c, va_arg(args, unsigned int));
+                break;
+            case 'X':
+                _buf.c += parse_uint_hex(_buf.c, va_arg(args, unsigned long));
+                break;
+            case 'u':
+                _buf.c += parse_uint(_buf.c, va_arg(args, unsigned int));
+                break;
+            case 'U':
+                _buf.c += parse_uint(_buf.c, va_arg(args, unsigned long));
+                break;
+            case 'f': case 'F':
+                dbl = va_arg(args, double);
+                if(dbl) _buf.c += parse_double(_buf.c, dbl);
+                else *_buf.c++ = '0';
+                break; 
+            case '%':
+                *_buf.c++ = '%';
+                break;
         }
+        ++_fmt.c;
     }
-    if(pos != 0) write(STDOUT, buf, pos);
+    write(STDOUT, buf, _buf.c-buf);
     va_end(args);
 }
 
